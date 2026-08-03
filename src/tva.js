f9db32c5b1d2f7ade3a4a0fa1c936ae419ef9480
Duflot <jeremy60.duflot60@gmail.com>
2026-08-03 15:16:37 +0200

    feat: ajout du taux de TVA reduit par defaut

--- src/tva.js
+++ src/tva.js
@@ -1,4 +1,4 @@
-function calculTTC(ht, taux) {
+function calculTTC(ht, taux = 0.2) {
   if (typeof ht !== "number" || ht < 0) throw new Error("montant invalide");
   return Math.round(ht * (1 + taux) * 100) / 100;
 }
